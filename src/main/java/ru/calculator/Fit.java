02f7d204155ff57a61ceb190401ab58ade6b335a
TrFilipp <trishin989898@gmail.com>
2024-05-08 22:49:56 +0300

    1.10 Идеальный вес

--- src/main/java/ru/calculator/Fit.java
+++ src/main/java/ru/calculator/Fit.java
@@ -13,8 +13,8 @@ public class Fit {
     public static void main(String[] args) {
         short height = 187;
         double man = manWeight(height);
-        System.out.println("Man 187 is " + man);
         double woman = womanWeight(height);
+        System.out.println("Man 187 is " + man);
         System.out.println("Woman 187 is " + woman);
     }
 }
