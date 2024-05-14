6fe73175be3485d7426ad29f101b4102691a78dd
TrFilipp <trishin989898@gmail.com>
2024-05-14 20:19:11 +0300

    3.3 Операторы сравнения в String

---
+++ src/main/java/ru/condition/Game.java
@@ -0,0 +1,17 @@
+package ru.condition;
+
+public class Game {
+    public static void menu(String name) {
+        if ("super mario".equals(name)) {
+            System.out.println("Start - super mario");
+        } else if ("tanks".equals(name)) {
+            System.out.println("Start - tanks");
+        } else if ("tetris".equals(name)) {
+            System.out.println("Start - tetris");
+        }
+    }
+
+    public static void main(String[] args) {
+        menu("tanks");
+    }
+}
