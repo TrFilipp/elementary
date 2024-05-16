f5a99df35a14ae447e83c9adfb24a0cf4665907f
TrFilipp <trishin989898@gmail.com>
2024-05-16 13:37:26 +0300

    3.7 Тернарное сравнение

---
+++ src/main/java/ru/condition/Max.java
@@ -0,0 +1,12 @@
+package ru.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        return left > right ? left : right;
+    }
+
+    public static void main(String[] args) {
+        int result = max(2, 3);
+        System.out.println(result);
+    }
+}
