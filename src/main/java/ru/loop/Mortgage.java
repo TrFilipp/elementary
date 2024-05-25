a880f93b66844d9c011f3b5c6aa19f8a2a3b0209
TrFilipp <trishin989898@gmail.com>
2024-05-25 23:10:30 +0300

    4.10 Ипотека

--- src/main/java/ru/loop/Mortgage.java
+++ src/main/java/ru/loop/Mortgage.java
@@ -3,8 +3,8 @@ package ru.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
+        percent /= 100;
         while (amount > 0) {
-            percent /= 100;
             amount += amount * percent;
             amount -= salary;
             year++;
