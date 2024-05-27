b27173b196213eb2c3accf269bf07cfb9ffbc2f4
TrFilipp <trishin989898@gmail.com>
2024-05-27 20:08:46 +0300

    5.2 Размер массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println(ages.length);
         String[] surname = new String[100500];
+        System.out.println(surname.length);
         float[] prices = new float[40];
+        System.out.println(prices.length);
     }
 }
