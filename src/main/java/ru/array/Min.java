2d04944c8fcc8b072d0795fa6b4618ab84a5c690
TrFilipp <trishin989898@gmail.com>
2024-06-01 16:19:47 +0300

    5.19 Поиск минимального числа в массиве

--- src/main/java/ru/array/Min.java
+++ src/main/java/ru/array/Min.java
@@ -3,7 +3,7 @@ package ru.array;
 public class Min {
     public static int findMin(int[] array) {
         int min = array[0];
-        for (int i = 0; i <= array.length - 1; i++) {
+        for (int i = 1; i < array.length; i++) {
             if (min > array[i]) {
                 min = array[i];
             }
