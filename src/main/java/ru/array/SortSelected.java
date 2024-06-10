64117ae5f98534e61ab616f7b2011790d87ae81a
TrFilipp <trishin989898@gmail.com>
2024-06-10 19:47:55 +0300

    Задание 5.21 Сортировка выборкой

--- src/main/java/ru/array/SortSelected.java
+++ src/main/java/ru/array/SortSelected.java
@@ -2,14 +2,10 @@ package ru.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-        int min = 0;
-        int minIndex = 0;
         for (int i = 0; i < data.length; i++) {
-            min = MinDiapason.findMin(data, i, data.length - 1);
-            minIndex = FindLoop.indexInRange(data, min, i, data.length - 1);
-            if (data[minIndex] < data[i]) {
-                SwitchArray.swap(data, minIndex, i);
-            }
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int minIndex = FindLoop.indexInRange(data, min, i, data.length - 1);
+            SwitchArray.swap(data, minIndex, i);
         }
         return data;
     }
