5337c73418cbfde153d0985bbbb9ca816964e1f1
TrFilipp <trishin989898@gmail.com>
2024-07-24 19:05:45 +0300

    Задание 7.15 Перегрузить метод max для трёх чисел

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -2,45 +2,20 @@ package ru.condition;
 
 public class Max {
 
+    public static int max(int left, int right) {
+        return left > right ? left : right;
+    }
+
     public int max(int first, int second, int third) {
-       int result = first;
-       if (third > first && third > second) {
-           result = third;
-       }
-       if (second > first && second > third) {
-           result = second;
-       }
-       if (first == second && first > third) {
-           result = second;
-       } else if (second == third && second > first) {
-           result = third;
-       }
-       return result;
+       return max(max(first, second), third);
     }
 
     public int max(int first, int second, int third, int forth) {
-        int result = first;
-        if (third > first && third > second && third > forth) {
-            result = third;
-        }
-        if (second > first && second > third && second > forth) {
-            result = second;
-        }
-        if (forth > first && forth > second && forth > third) {
-            result = forth;
-        }
-        if (first == second && first > third && first > forth) {
-            result = second;
-        } else if (second == third && second > first && second > forth) {
-            result = third;
-        } else if (third == forth && third > first && third > second) {
-            result = forth;
-        }
-        return result;
+        return max(max(first, second), max(third, forth));
     }
 
     public static void main(String[] args) {
-       Max maximum = new Max();
+      Max maximum = new Max();
        System.out.println(maximum.max(1, 5, 3));
        System.out.println(maximum.max(4, 2, 7, 1));
     }
